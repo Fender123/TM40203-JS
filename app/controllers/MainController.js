e2f516c9ff0c06ce98628fdddc0a75aa4d6604d2
Fender123 <pal.fender+git@gmail.com>
2015-11-24 20:35:35 +0100

    fixed absolute urls in controller template

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -4,7 +4,7 @@ angular.module('App.main', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
-            templateUrl: '/app/views/main.html',
+            templateUrl: 'app/views/main.html',
             controller: 'MainCtrl'
         });
     }])
